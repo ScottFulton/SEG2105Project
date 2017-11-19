10a68ac8e2a5358fec7b7c46b8b40f3418bf0e08
Vioarr <vokeybe@gmail.com>
2017-11-18 20:05:06 -0500

    help

--- HouseholdChoreManager/app/src/main/java/project/seg/householdchoremanager/MainActivity.java
+++ HouseholdChoreManager/app/src/main/java/project/seg/householdchoremanager/MainActivity.java
@@ -3,11 +3,11 @@ package project.seg.householdchoremanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TitleActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
+        setContentView(R.layout.activity_main);
     }
 }
