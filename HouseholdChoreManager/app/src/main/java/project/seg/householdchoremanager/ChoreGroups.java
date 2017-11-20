56958c26328bcd4f253e81211226bce606e17f92
7chris71000 <c.francis7@live.ca>
2017-11-20 16:23:10 -0500

    Worked on groups UI

--- HouseholdChoreManager/app/src/main/java/project/seg/householdchoremanager/ChoreGroups.java
+++ HouseholdChoreManager/app/src/main/java/project/seg/householdchoremanager/ChoreGroups.java
@@ -2,12 +2,20 @@ package project.seg.householdchoremanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ChoreGroups extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chore_groups);
-    }
+        setContentView(R.layout.activity_main);
+
+
+        String[] choreList = {"Walk Dog", "Do the Dishes", "Clean Room", "Make Bed", "Take Trash Out"};
+        ListView listView = (ListView) findViewById(R.id.list);
+        ChoreCustomAdapter adapter = new ChoreCustomAdapter(this,choreList);
+        listView.setAdapter(adapter);
+    });
+}
 }
